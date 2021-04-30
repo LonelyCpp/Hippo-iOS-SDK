16365e52db78d3413abc798af388335dc0565615
Ananthu Kanive <ananthu.kanive@smallcase.com>
2021-04-30 10:01:58 +0530

    remoce appicons, add bridging header

---
+++ Hippo/Hippo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Hippo-Bridging-Header.h
+//  Hippo
+//
+//  Created by apple on 30/04/21.
+//  Copyright © 2021 CL-macmini-88. All rights reserved.
+//
+
+#ifndef Hippo_Bridging_Header_h
+#define Hippo_Bridging_Header_h
+
+
+#endif /* Hippo_Bridging_Header_h */
